03ab0828226594d376a55666293c0475e2a0dd9d
SilverVenom <akhil31.ad@gmail.com>
2021-01-14 15:10:13 +0530

    Lab8

---
+++ ADS/LabWork/Lab8/src/com/app/core/TreeNode.java
@@ -0,0 +1,92 @@
+//Created by @AkhilD on 14/01/2021.
+package com.app.core;
+public class TreeNode {
+
+	public class BTnode {
+		int data;
+		BTnode lChild;
+		BTnode rChild;
+
+	}
+
+	private BTnode root;
+
+	public TreeNode() {
+		root = null;
+	}
+
+	public void create() {
+		root = new BTnode();
+		root.data = 7;
+		root.lChild = new BTnode();
+		root.lChild.data = 9;
+		root.lChild.lChild = new BTnode();
+		root.lChild.lChild.data = 1;
+		root.lChild.rChild = new BTnode();
+		root.lChild.rChild.data = 5;
+		root.lChild.rChild.lChild = new BTnode();
+		root.lChild.rChild.lChild.data = 3;
+
+		root.rChild = new BTnode();
+		root.rChild.data = 4;
+		root.rChild.rChild = new BTnode();
+		root.rChild.rChild.data = 2;
+		root.rChild.rChild.lChild = new BTnode();
+		root.rChild.rChild.lChild.data = 6;
+	}
+
+	public void preOrder(BTnode node) {
+		if (node == null) {
+			return;
+		}
+		System.out.print(node.data + " ");
+		if (node.lChild != null) {
+			preOrder(node.lChild);
+		}
+		if (node.rChild != null) {
+			preOrder(node.rChild);
+		}
+
+	}
+
+	public void postOrder(BTnode node) {
+		if (node == null) {
+			return;
+		}
+
+		if (node.lChild != null) {
+			preOrder(node.lChild);
+		}
+		if (node.rChild != null) {
+			preOrder(node.rChild);
+		}
+		System.out.print(node.data + " ");
+	}
+
+	public void InOrder(BTnode node) {
+		if (node == null) {
+			return;
+		}
+
+		if (node.lChild != null) {
+			preOrder(node.lChild);
+			System.out.print(node.data + " ");
+		}
+		if (node.rChild != null) {
+			preOrder(node.rChild);
+		}
+
+	}
+
+	public void printList() {
+		System.out.println("PreOrder: ");
+		preOrder(root);
+		System.out.println();
+
+		System.out.println("PostOrder: ");
+		postOrder(root);
+		System.out.println();
+		System.out.println("InOrder: ");
+		InOrder(root);
+	}
+}
---
+++ ADS/LabWork/Lab8/src/com/app/testing/TreeTesting.java
@@ -0,0 +1,17 @@
+//Created by @AkhilD on 14/01/2021.
+package com.app.testing;
+
+import com.app.core.TreeNode;
+
+public class TreeTesting {
+
+	public static void main(String[] args) {
+
+		TreeNode t = new TreeNode();
+		
+		t.create();
+		t.printList();
+		
+	}
+
+}
