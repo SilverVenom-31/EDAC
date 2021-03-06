b06c5719dfe4bb95f9cee89c67e877d3921724e1
SilverVenom-31 <akhil31.ad@gmail.com>
2021-03-06 20:23:20 +0530

    Testing git config in IDE

--- ADVJAVA/LabWork/FlightReservationSystem/src/main/java/com/flightmgmt/FlightReservationSystemApplication.java
+++ ADVJAVA/LabWork/FlightReservationSystem/src/main/java/com/flightmgmt/FlightReservationSystemApplication.java
@@ -2,7 +2,7 @@ package com.flightmgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//testing for git
 @SpringBootApplication
 public class FlightReservationSystemApplication {
 
